94c52e9af27eb7fd060f33f3673836bdc75c3b62
sureshkmss <30689594+sureshkmss@users.noreply.github.com>
2017-08-08 15:51:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName', function (req, res) {
+    var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 app.get('/ui/style.css', function (req, res) {
