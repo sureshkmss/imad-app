f521311d769adf22da7f338c2a3289e83abef59e
sureshkmss <30689594+sureshkmss@users.noreply.github.com>
2017-08-17 16:49:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ function createTemplate(data){
             <h2>
             ${heading}
             </h2>
-            <div>
+            <div id='l'>
             ${date}
             </div>
             <div>
